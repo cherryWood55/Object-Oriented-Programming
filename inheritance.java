190763271ea4a96d4c481a5cda508aa0ebe0bc18
shehani009 <shehanikandy@yahoo.com>
2019-10-14 19:32:28 +0530

    added inheritance

---
+++ inheritance.java
@@ -0,0 +1,18 @@
+class Teacher {
+   String designation = "Teacher";
+   String collegeName = "Beginnersbook";
+   void does(){
+	System.out.println("Teaching");
+   }
+}
+
+public class PhysicsTeacher extends Teacher{
+   String mainSubject = "Physics";
+   public static void main(String args[]){
+	PhysicsTeacher obj = new PhysicsTeacher();
+	System.out.println(obj.collegeName);
+	System.out.println(obj.designation);
+	System.out.println(obj.mainSubject);
+	obj.does();
+   }
+}
